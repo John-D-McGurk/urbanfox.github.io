ccecb8951d750a5cc8e1939ebf51af97760c2e2e
John McGurk <jomcg6099@uit.no>
2024-01-12 07:43:52 +0100

    stable, started parallax scroll effect

--- static/app.js
+++ static/app.js
@@ -14,8 +14,21 @@ function scaling() {
 
 }
 
-function sidebarContentFade(maskContainer, maskFadeDistance) {
-    const maskFadeThreshold = 35;
+function logoShrink() {
+const logo = document.querySelector('#logo');
+
+
+    if (window.scrollY > 50) {
+        logo.style.cssText = 'top: 0%; left: 0%; height: 6rem; transform: translate(1rem, 1rem);'
+    } else {
+        logo.style.cssText = ''
+    }
+};
+
+function contentFade(maskContainer, maskFadeDistance) {
+    const maskFadeThreshold = 20;
+
+
   maskContainer.style.cssText = `-webkit-mask:  linear-gradient( to bottom,
     rgba(0, 0, 0 ,1) 0px,
     rgba(0, 0, 0 ,1) ${maskFadeDistance - maskFadeThreshold}px,
@@ -25,28 +38,30 @@ function sidebarContentFade(maskContainer, maskFadeDistance) {
 function sidebarEffects(sidebar, bg) {
   const sidebarSocials = document.querySelector("#sidebar-socials");
   const maskContainer = document.querySelector("#mask-container");
+  const sidebarBG = document.querySelector('#sidebar-bg')
 
   let distanceToBGLock = maskContainer.offsetHeight - window.innerHeight;
   let distanceTilMask = -1 * (window.scrollY + window.innerHeight - sidebarSocials.offsetHeight - maskContainer.offsetHeight);
   let maskFadeDistance = maskContainer.offsetHeight - distanceTilMask;
 
-
+  sidebarBG.style.height = `${maskContainer.offsetHeight}px`
 
   if (window.scrollY >= distanceToBGLock) {
-    sidebarSocials.style.cssText = `position: absolute; top: ${sidebar.offsetHeight - sidebarSocials.offsetHeight}px; width: ${sidebar.offsetWidth}`;
+    sidebarSocials.style.cssText = `position: absolute; top: ${sidebarBG.offsetHeight - sidebarSocials.clientHeight - 30}px; width: ${sidebar.offsetWidth}`;
     
     bg.forEach(
       (element) =>
         (element.style.cssText = `position: absolute; top: ${distanceToBGLock}px;`)
     );
 
-  } else {    
+  } else {       
+    contentFade(maskContainer, maskFadeDistance); 
     sidebarSocials.style.cssText = "";
     bg.forEach((element) => (element.style.cssText = ""));
 
-    if (distanceTilMask > -sidebarSocials.offsetHeight) {
-      sidebarContentFade(maskContainer, maskFadeDistance);
-    }
+    
+      
+    
 
   }
 }
@@ -58,11 +73,15 @@ function resizeScrollCorrections() {
   ];
   const sidebar = document.querySelector("#sidebar");
 
+  logoShrink();
   sidebarEffects(sidebar, bg);
   scaling();
 }
 
+document.addEventListener('DOMContentLoaded', () => {
 
+    resizeScrollCorrections();
+});
 
 
 window.onscroll = function () {
